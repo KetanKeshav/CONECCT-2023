f840770d8c79a36ee1c36e0e327aab3a5bd2de38
Ketan Keshav <ketan.keshav7@gmail.com>
2023-02-15 19:23:33 +0530

    Track U CFP

--- tracks.php
+++ tracks.php
@@ -203,7 +203,7 @@
                             <td>CPS for Aerospace Systems</td>
                             <td>Puneet Kumar Mishra</td>
                             <td>mishra_puneet@ieee.org</td>
-                            <td>-</td>
+                            <td><a href="./downloads/Track U.pdf" download="CONECCT 2023 CFP Track - U" class="btn btn-primary py-sm-3 px-sm-5 rounded-pill">Download</a></td>
                             <!-- <td><a href="./downloads/Track U.pdf" download="CONECCT 2023 CFP Track - U" class="btn btn-primary py-sm-3 px-sm-5 rounded-pill">Download</a></td> -->
                         </tr>
                     </table>
