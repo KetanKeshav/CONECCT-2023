36534ef13c4a83cd94b3602d5ce849763521d821
Ketan Keshav <ketan.keshav7@gmail.com>
2023-06-22 18:39:52 +0530

    Schedule update

--- include/navbar.php
+++ include/navbar.php
@@ -15,11 +15,20 @@
                 <a href="oc.php" class="nav-item nav-link  <?php if($page=="oc") echo 'active'?>">Organizing Committee</a>
                 <a href="index.php#speakers" class="nav-item nav-link">Speakers</a>
                 <a href="index.php#tutorials" class="nav-item nav-link">Tutorials</a>
-                <a href="tracks.php" class="nav-item nav-link  <?php if($page=="tracks") echo 'active'?>">Tracks</a>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle <?php if($page=="schedule") echo 'active'?>" href="#" id="scheduleDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Schedule
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="scheduleDropdown">
+                        <a class="dropdown-item" href="downloads/conference_schedule.pdf" download>Conference Schedule</a>
+                        <a class="dropdown-item" href="downloads/track_session_details.pdf" download>Track Session Details</a>
+                    </div>
+                </li>
                 <a href="cfp.php" class="nav-item nav-link  <?php if($page=="cfp") echo 'active'?>">Call For Papers</a>
                 <a href="https://yp.ieeebangalore.org/conecct-yp-meet-2023/" target="_blank" class="nav-item nav-link ">YP Meet</a>
             </div>
             <a href="https://in.explara.com/e/ieee--conecct-2023" class="btn rounded-pill py-2 px-4 ms-3 d-none d-lg-block">Register Now</a>
+            <a href="downloads/CONECCT_2023" download>Author slides</a>
         </div>
     </nav>
 </div>
